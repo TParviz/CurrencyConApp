bf52ebcdfb58ea16e304f3172c35d25833a1df97
aZAIL <jokerkod01@gmail.com>
2020-07-14 17:14:44 +0300

    * improvements

--- app/src/main/java/com/example/currencyconapp/CurrencyCalculatorActivity.java
+++ app/src/main/java/com/example/currencyconapp/CurrencyCalculatorActivity.java
@@ -23,20 +23,48 @@ import android.widget.TextView;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
+import androidx.recyclerview.widget.RecyclerView;
 
 import org.w3c.dom.Text;
 
 import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Pattern;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.simplexml.SimpleXmlConverterFactory;
 
 public class CurrencyCalculatorActivity extends AppCompatActivity {
 
     List<Valute> dataset;
 
+    RecyclerView recyclerView;
+    List<Valute> valuteList;
+    List<String> namesList = new ArrayList<>();
+    RecyclerAdapter recyclerAdapter;
+    Spinner spinner1;
+    TextView textView;
+    List<Double> valueList = new ArrayList<>();
+    Map<String, Double> hashMap = new HashMap<String, Double>();
 
-    // String[] data1 = {"Афганский Афган", "Армянский Драм", "Беларусский Рубль", "Датский Крон", "Британский Фунт"};
+
+
+
+
     Button btnone;
     Button btntwo;
     Button btnthree;
@@ -58,6 +86,7 @@ public class CurrencyCalculatorActivity extends AppCompatActivity {
 
 
     View view = this.getCurrentFocus();
+    private Double currencyVal;
 
     /*private DecimalFormat decimalFormat;
     private double valueOne = Double.NaN;
@@ -72,12 +101,15 @@ public class CurrencyCalculatorActivity extends AppCompatActivity {
         setContentView(R.layout.calculator_layout);
 
         String[] valuteNames = getIntent().getStringArrayExtra("valuteNamesList");
-
+        spinner1 = (Spinner) findViewById(R.id.spinner_two);
+/*
         ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, valuteNames);
         adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         Spinner spinner1 = (Spinner) findViewById(R.id.spinner_two);
-        spinner1.setAdapter(adapter1);
+        spinner1.setAdapter(adapter1);*/
+        final MutableLiveData<List<Valute>> names = new MutableLiveData<>();
+
 
         btnone = (Button) findViewById(R.id.btn_one);
         image = (ImageView) findViewById(R.id.second_image_converter_layout);
@@ -110,17 +142,91 @@ public class CurrencyCalculatorActivity extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable s) {
-                float valuteInt = 0;
+//                double valuteInt = 0;
+//
+//                if (txtInput.getText().length() > 0) {
+//                    valuteInt = Double.valueOf(txtOutput.getText().toString());
+//                }
+                String s1 = "0.0";
+                if(!s.toString().isEmpty())
+                    s1 = s.toString();
+                double output = Math.round(Double.parseDouble(s1) * currencyVal * 100.0) / 100.0;
+                txtOutput.setText(Double.toString(output));
 
-                if (txtInput.getText().length() > 0) {
-                    valuteInt = Float.valueOf(txtInput.getText().toString());
+            }
+        });
+
+        names.observe(this, new Observer<List<Valute>>() {
+            @Override
+            public void onChanged(List<Valute> valutes) {
+                namesList.clear();
+                for (int i = 0; i < valutes.size(); i++) {
+
+                    namesList.add(valutes.get(i).getName());
 
                 }
-                if (valuteInt > 0) {
-                    txtOutput.setText(Float.toString(valuteInt * 69.78f));
+
+
+                Log.d("myLogs", namesList.toString());
+
+                ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(CurrencyCalculatorActivity.this, android.R.layout.simple_spinner_item, namesList);
+                adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+                Spinner spinner1 = (Spinner) findViewById(R.id.spinner_two);
+                spinner1.setAdapter(adapter1);
+
+                // recyclerAdapter.submitList(valutes);
+            }
+
+        });
+
+        names.observe(this, new Observer<List<Valute>>() {
+            @Override
+            public void onChanged(List<Valute> valutes) {
+                for (Valute v : valutes) {
+                    hashMap.put(v.getName(), v.getValue());
                 }
             }
         });
+
+
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .callTimeout(30, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .build();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://nbt.tj/")
+                .client(client)
+                .addConverterFactory(SimpleXmlConverterFactory.create())
+                .build();
+
+
+        Date c = Calendar.getInstance().getTime();
+        System.out.println("Current time => " + c);
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+        String formattedDate = df.format(c);
+        Log.d("CURRENT_DATE", "" + formattedDate);
+        MyServiceXML myServiceXML = retrofit.create(MyServiceXML.class);
+        Call<Valcurs> call = myServiceXML.getMyService(formattedDate);
+
+        call.enqueue(new Callback<Valcurs>() {
+            @Override
+            public void onResponse(Call<Valcurs> call, Response<Valcurs> response) {
+                List<Valute> valuteList = response.body().valuteList;
+
+                Log.d("FILTERED_LIST", "" + valuteList.toString());
+                names.postValue(valuteList);
+
+            }
+            @Override
+            public void onFailure(Call<Valcurs> call, Throwable t) {
+
+
+                Log.d("TASK_RESP1", "" + t);
+            }
+        });
+
 
 
         btnone.setOnClickListener(new View.OnClickListener() {
@@ -246,9 +352,60 @@ public class CurrencyCalculatorActivity extends AppCompatActivity {
 
                 String selectedName = parent.getSelectedItem().toString();
                 Log.d("SELECTED_NAME", "" + selectedName);
+                currencyVal = hashMap.get(selectedName);
+
+
+
+
+                if (selectedName.equals("Доллари ИМА")) {
+                    image.setImageResource(R.drawable.usa);
+                }
+                if (selectedName.equals("ЕВРО")) {
+                    image.setImageResource(R.drawable.europa);
+                }
+                if (selectedName.equals("Юани Чин")) {
+                    image.setImageResource(R.drawable.china);
+                }
+                if (selectedName.equals("Франки Швейтсария")) {
+                    image.setImageResource(R.drawable.swiss);
+                }
+                /*if (selectedName.equals("Доллари ИМА")) {
+                    image.setImageResource(R.drawable.england);
+                }
                 if (selectedName.equals("Доллари ИМА")) {
                     image.setImageResource(R.drawable.england);
                 }
+                if (selectedName.equals("Доллари ИМА")) {
+                    image.setImageResource(R.drawable.england);
+                }
+                if (selectedName.equals("Доллари ИМА")) {
+                    image.setImageResource(R.drawable.england);
+                }
+                if (selectedName.equals("Доллари ИМА")) {
+                    image.setImageResource(R.drawable.england);
+                }
+                if (selectedName.equals("Доллари ИМА")) {
+                    image.setImageResource(R.drawable.england);
+                }
+                if (selectedName.equals("Доллари ИМА")) {
+                    image.setImageResource(R.drawable.england);
+                }
+                if (selectedName.equals("Доллари ИМА")) {
+                    image.setImageResource(R.drawable.england);
+                }
+                if (selectedName.equals("Доллари ИМА")) {
+                    image.setImageResource(R.drawable.england);
+                }
+                if (selectedName.equals("Доллари ИМА")) {
+                    image.setImageResource(R.drawable.england);
+                }
+                if (selectedName.equals("Доллари ИМА")) {
+                    image.setImageResource(R.drawable.england);
+                }
+                if (selectedName.equals("Доллари ИМА")) {
+                    image.setImageResource(R.drawable.england);
+                }*/
+
 
             }
 
@@ -257,25 +414,6 @@ public class CurrencyCalculatorActivity extends AppCompatActivity {
             }
         });
 
-        txtInput.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                EditText edittext = (EditText) v;
-                int inType = edittext.getInputType();       // Backup the input type
-                edittext.setInputType(InputType.TYPE_NULL); // Disable standard keyboard
-
-                edittext.onTouchEvent(event);               // Call native handler
-
-                float x = event.getX();
-                float y = event.getY();
-                int touchPosition = edittext.getOffsetForPosition(x, y);
-                if (touchPosition > 0) {
-                    edittext.setSelection(touchPosition);
-                }
-                edittext.setInputType(inType);              // Restore input type
-                return true; // Consume touch event
-            }
-        });
     }
 
 
